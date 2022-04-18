bc6ebd2b70834f5364986f315db900d502f81e5a
Denis Bogdanov <denx-b@mail.ru>
2022-04-18 15:38:04 +0200

    PHP 7.0 support

--- src/Bitrix/Auth/Adapter/Telegram.php
+++ src/Bitrix/Auth/Adapter/Telegram.php
@@ -44,7 +44,7 @@ class Telegram extends Adapter
         script.src = "https://telegram.org/js/telegram-widget.js?2";
         document.getElementsByTagName("head")[0].appendChild(script);
         </script>
-        HTML;
+HTML;
     }
 
     /**
